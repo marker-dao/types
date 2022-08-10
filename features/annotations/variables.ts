f0ecc89ceb53d219066ffa4ffceb3dfe445c266d
MertvyeDushi <>
2022-08-10 15:07:47 +0400

    Object literal annotations

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,10 +1,30 @@
+/**
+ * Variables
+ */
 const numberVar: number = 5
 const stringVar: string = 'fast'
 const booleanVar: boolean = false
 const nullVar: null = null
 const undefinedVar: undefined = undefined
-
 /**
  * Built-in objects
  */
-const now: Date = new Date()
+const dateInstance: Date = new Date()
+/**
+ * Arrays
+ */
+const stringArray: string[] = ['', '']
+const numberArray: number[] = [0, 1]
+/**
+ * Classes
+ */
+class ExampleClass {}
+
+const ExampleClassInstance: ExampleClass = new ExampleClass()
+/**
+ * Object literals
+ */
+const point: { x: number; y: number } = {
+  x: 20,
+  y: 20,
+}
