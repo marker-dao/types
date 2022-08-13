41a560e51a5ed71925427f365b2691918d48b366
MertvyeDushi <>
2022-08-13 18:02:27 +0400

    Void && Never

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -9,3 +9,29 @@ function divide (a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b
 }
+/**
+ * Void
+ */
+function logger (message: string): void {
+  console.log(message)
+}
+/**
+ * Never
+ */
+function throwError1 (message: string): never {
+  throw new Error(message)
+}
+
+function throwError2 (message: string): string {
+  if (message) {
+    throw new Error(message)
+  }
+  
+  return ''
+}
+
+function throwError3 (message: string): void {
+  if (message) {
+    throw new Error(message)
+  }
+}
