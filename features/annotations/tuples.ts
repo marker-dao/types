5c6b1b20410730567d7e21e050efab319676f449
MertvyeDushi <>
2022-08-13 22:41:57 +0400

    Tuples

---
+++ features/annotations/tuples.ts
@@ -0,0 +1,36 @@
+/**
+ * Object
+ */
+const drinkDefault = {
+  color: 'transperent',
+  carbonated: false,
+  sugar: 0,
+}
+/**
+ * Tuple
+ */
+const cola: [string, boolean, number] = [
+  'brown',
+  true,
+  40,
+]
+/**
+ * Types
+ */
+type Drink = [string, boolean, number]
+
+const sprite: Drink = [
+  'transperent',
+  true,
+  40,
+]
+
+const cofee: Drink = [
+  'black',
+  false,
+  5,
+]
+/**
+ * Example
+ */
+const specification: [number, number ] = [400, 3310]
