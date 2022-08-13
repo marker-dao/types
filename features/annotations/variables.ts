89b61a5aab682ed78ccf08424327458044bedee7
MertvyeDushi <>
2022-08-13 17:30:38 +0400

    Var annotations

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -28,3 +28,34 @@ const point: { x: number; y: number } = {
   x: 20,
   y: 20,
 }
+/**
+ * Functions
+ */
+const functionAnnotation1: (i: number) => void = (i: number) =>  {
+  console.log(i)
+}
+
+function functionAnnotation2 (i: number): void {
+  console.log(i)
+}
+/**
+ * Returns any type
+ */
+const json = '{"x": 10, "y": 10}'
+const coordinates: { x: number; y: number } = JSON.parse(json)
+/**
+ * Declare variable without initialisation
+ */
+let isVisible: boolean
+
+isVisible = true
+/**
+ * Some available types
+ */
+let i: number | string
+
+if (point.x !== numberVar) {
+  i = 5
+} else {
+  i = '5'
+}
