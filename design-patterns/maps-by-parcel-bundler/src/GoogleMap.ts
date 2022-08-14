ec06c5daf0e731821cc0e4abb6eefbb96f870ec8
MertvyeDushi <>
2022-08-14 23:22:46 +0400

    @maps: Refactor && Add Marker

--- design-patterns/maps-by-parcel-bundler/src/GoogleMap.ts
+++ design-patterns/maps-by-parcel-bundler/src/GoogleMap.ts
@@ -1,3 +1,8 @@
+interface Location {
+  lat: number
+  lng: number
+}
+
 export class GoogleMap {
   private googleMap: google.maps.Map
 
@@ -7,10 +12,22 @@ export class GoogleMap {
         lat: 0,
         lng: 0,
       },
-      zoom: 1,
+      zoom: 2,
     }
     const el = document.getElementById(elementId) as HTMLElement
 
     this.googleMap = new google.maps.Map(el, options)
+  }
+
+  addMarker (location: Location): void {
+    const opts = {
+      map: this.googleMap,
+      position: {
+        lat: location.lat,
+        lng: location.lng,
+      }
+    }
+
+    new google.maps.Marker(opts)
   }
 }
--- design-patterns/maps-by-parcel-bundler/src/index.ts
+++ design-patterns/maps-by-parcel-bundler/src/index.ts
@@ -2,6 +2,9 @@ import { User } from './User'
 import { Company } from './Company'
 import { GoogleMap } from './GoogleMap'
 
-new User()
-new Company()
-new GoogleMap('map')
+const user = new User()
+const company = new Company()
+const googleMap = new GoogleMap('map')
+
+googleMap.addMarker(user.location)
+googleMap.addMarker(company.location)
